773e39cd49bf4f9c37ae5984c9031f958274c738
Potter??? <103591534+Dandorime@users.noreply.github.com>
2024-12-16 17:54:08 +0300

    upd server

--- src/index.ts
+++ src/index.ts
@@ -13,59 +13,59 @@ dotenv.config();
 // Your secret bot token.
 const token = process.env.TELEGRAM_BOT_TOKEN;
 
-// /**
-//  * Sets init data in the specified Response object.
-//  * @param res - Response object.
-//  * @param initData - init data.
-//  */
-// function setInitData(res: Response, initData: any): void {
-//   res.locals.initData = initData;
-// }
+/**
+ * Sets init data in the specified Response object.
+ * @param res - Response object.
+ * @param initData - init data.
+ */
+function setInitData(res: Response, initData: any): void {
+  res.locals.initData = initData;
+}
 
-// /**
-//  * Extracts init data from the Response object.
-//  * @param res - Response object.
-//  * @returns Init data stored in the Response object. Can return undefined in case,
-//  * the client is not authorized.
-//  */
-// function getInitData(res: Response): any | undefined {
-//   return res.locals.initData;
-// }
+/**
+ * Extracts init data from the Response object.
+ * @param res - Response object.
+ * @returns Init data stored in the Response object. Can return undefined in case,
+ * the client is not authorized.
+ */
+function getInitData(res: Response): any | undefined {
+  return res.locals.initData;
+}
 
-// /**
-//  * Middleware which authorizes the external client.
-//  * @param req - Request object.
-//  * @param res - Response object.
-//  * @param next - function to call the next middleware.
-//  */
-// const authMiddleware: RequestHandler = (req, res, next) => {
-//   // We expect passing init data in the Authorization header in the following format:
-//   // <auth-type> <auth-data>
-//   // <auth-type> must be "tma", and <auth-data> is Telegram Mini Apps init data.
-//   const [authType, authData = ''] = (req.header('authorization') || '').split(' ');
-//   console.log(authData);
+/**
+ * Middleware which authorizes the external client.
+ * @param req - Request object.
+ * @param res - Response object.
+ * @param next - function to call the next middleware.
+ */
+const authMiddleware: RequestHandler = (req, res, next) => {
+  // We expect passing init data in the Authorization header in the following format:
+  // <auth-type> <auth-data>
+  // <auth-type> must be "tma", and <auth-data> is Telegram Mini Apps init data.
+  const [authType, authData = ''] = (req.header('authorization') || '').split(' ');
+  console.log(authData);
 
-//   // switch (authType) {
-//   //   case 'tma':
-//   //     try {
-//   //       // Validate init data.
-//   //       validate(authData, token as string, {
-//   //         // We consider init data sign valid for 1 hour from their creation moment.
-//   //         expiresIn: 3600,
-//   //       });
+  // switch (authType) {
+  //   case 'tma':
+  //     try {
+  //       // Validate init data.
+  //       validate(authData, token as string, {
+  //         // We consider init data sign valid for 1 hour from their creation moment.
+  //         expiresIn: 3600,
+  //       });
 
-//   //       // Parse init data. We will surely need it in the future.
-//   //       setInitData(res, parse(authData));
-//   //       return next();
-//   //     } catch (e) {
-//   //       return next(e);
-//   //     }
-//   //   // ... other authorization methods.
-//   //   default:
-//   //     return next(new Error('Unauthorized'));
-//   // }
-//   return next();
-// };
+  //       // Parse init data. We will surely need it in the future.
+  //       setInitData(res, parse(authData));
+  //       return next();
+  //     } catch (e) {
+  //       return next(e);
+  //     }
+  //   // ... other authorization methods.
+  //   default:
+  //     return next(new Error('Unauthorized'));
+  // }
+  return next();
+};
 
 // /**
 //  * Middleware which shows the user init data.
@@ -73,15 +73,15 @@ const token = process.env.TELEGRAM_BOT_TOKEN;
 //  * @param res - Response object.
 //  * @param next - function to call the next middleware.
 //  */
-// const showInitDataMiddleware: RequestHandler = (_req, res, next) => {
-//   const initData = getInitData(res);
-//   if (!initData) {
-//     return next(new Error('Cant display init data as long as it was not found'));
-//   }
-//   console.log(initData);
+const showInitDataMiddleware: RequestHandler = (_req, res, next) => {
+  const initData = getInitData(res);
+  if (!initData) {
+    return next(new Error('Cant display init data as long as it was not found'));
+  }
+  console.log(initData);
   
-//   res.json(initData);
-// };
+  res.json(initData);
+};
 
 // /**
 //  * Middleware which displays the user init data.
@@ -105,12 +105,12 @@ app.use(cors());
 
 app.use(express.json());
 app.use('/api', apiRouter);
-// app.use(authMiddleware);
-// app.get('/', showInitDataMiddleware);
+app.use(authMiddleware);
+app.post('/', showInitDataMiddleware);
 
-app.post('/', (req, res) => {
-  log('post ')
-})
+// app.post('/', (req, res) => {
+//   log('post ')
+// })
 // app.use(defaultErrorMiddleware);
 
 app.get('/', (req, res) => {
