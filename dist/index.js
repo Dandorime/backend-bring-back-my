caefe50ddc95157cdf6ee2b5861ef8f866852bd6
Potter??? <103591534+Dandorime@users.noreply.github.com>
2024-12-16 16:01:05 +0300

    build

--- dist/index.js
+++ dist/index.js
@@ -18,7 +18,7 @@ const db_connect_1 = require("./db/db_connect");
 const router_1 = __importDefault(require("./router"));
 const test_model_1 = __importDefault(require("./db/models/test.model"));
 const console_1 = require("console");
-const init_data_node_1 = require("@telegram-apps/init-data-node");
+// import { validate, parse } from '@telegram-apps/init-data-node';
 dotenv_1.default.config();
 // Your secret bot token.
 const token = process.env.TELEGRAM_BOT_TOKEN;
@@ -51,25 +51,25 @@ const authMiddleware = (req, res, next) => {
     // <auth-type> must be "tma", and <auth-data> is Telegram Mini Apps init data.
     const [authType, authData = ''] = (req.header('authorization') || '').split(' ');
     console.log(authData);
-    switch (authType) {
-        case 'tma':
-            try {
-                // Validate init data.
-                (0, init_data_node_1.validate)(authData, token, {
-                    // We consider init data sign valid for 1 hour from their creation moment.
-                    expiresIn: 3600,
-                });
-                // Parse init data. We will surely need it in the future.
-                setInitData(res, (0, init_data_node_1.parse)(authData));
-                return next();
-            }
-            catch (e) {
-                return next(e);
-            }
-        // ... other authorization methods.
-        default:
-            return next(new Error('Unauthorized'));
-    }
+    // switch (authType) {
+    //   case 'tma':
+    //     try {
+    //       // Validate init data.
+    //       validate(authData, token as string, {
+    //         // We consider init data sign valid for 1 hour from their creation moment.
+    //         expiresIn: 3600,
+    //       });
+    //       // Parse init data. We will surely need it in the future.
+    //       setInitData(res, parse(authData));
+    //       return next();
+    //     } catch (e) {
+    //       return next(e);
+    //     }
+    //   // ... other authorization methods.
+    //   default:
+    //     return next(new Error('Unauthorized'));
+    // }
+    return next();
 };
 /**
  * Middleware which shows the user init data.
